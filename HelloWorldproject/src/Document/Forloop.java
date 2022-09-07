15f6ae95e3f07dc8cd05e93ecdb211e7356213a6
Shubham <Shubham@LAPTOP-O1SSB60N>
2022-09-07 12:16:32 +0530

    first commit

---
+++ HelloWorldproject/src/Document/Forloop.java
@@ -0,0 +1,16 @@
+package Document;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+                 int num = 2;
+       
+                 for(int i=1;i<=10;i++)
+		{
+		  System.out.println(num * i);	
+		 
+     	}
+		 
+	}
+
+}
